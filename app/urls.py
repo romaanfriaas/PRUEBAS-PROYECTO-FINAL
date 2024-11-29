0d1601d151bd06a3933ed962e788c9e50c9fd219
unknown <romanfriasyf@gmail.com>
2024-11-29 08:49:19 -0300

    paginacion

--- app/urls.py
+++ app/urls.py
@@ -5,12 +5,12 @@ from . import views
 urlpatterns = [
     path('', views.index_page, name='index-page'),
     path('login/', views.index_page, name='login'),
-    # Actualizamos esta ruta para que acepte el parámetro `page`
     path('home/', views.home, name='home'),
-    path('home/<int:page>/', views.home, name='home_page'),  # Nueva ruta para manejar páginas específicas
     path('buscar/', views.search, name='buscar'),
 
     path('favourites/', views.getAllFavouritesByUser, name='favoritos'),
     path('favourites/add/', views.saveFavourite, name='agregar-favorito'),
     path('favourites/delete/', views.deleteFavourite, name='borrar-favorito'),
+
+    path('exit/', views.exit, name='exit'),
 ]
