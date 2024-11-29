98b6b1ee88320718e0116071f8a66005763e352b
unknown <romanfriasyf@gmail.com>
2024-11-29 09:00:58 -0300

    paginacionnn

--- app/urls.py
+++ app/urls.py
@@ -6,11 +6,9 @@ urlpatterns = [
     path('', views.index_page, name='index-page'),
     path('login/', views.index_page, name='login'),
     path('home/', views.home, name='home'),
+    path('home/<int:page>/', views.home, name='home_page'),  # Esta ruta maneja las páginas
     path('buscar/', views.search, name='buscar'),
-
     path('favourites/', views.getAllFavouritesByUser, name='favoritos'),
     path('favourites/add/', views.saveFavourite, name='agregar-favorito'),
     path('favourites/delete/', views.deleteFavourite, name='borrar-favorito'),
-
-    path('exit/', views.exit, name='exit'),
 ]
